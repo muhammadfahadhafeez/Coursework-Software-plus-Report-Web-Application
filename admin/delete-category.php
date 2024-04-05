fb9aa32fc25af77e22e9c845ba500f3b96bf230f
muhammadfahadhafeez <161144499+muhammadfahadhafeez@users.noreply.github.com>
2024-04-05 22:35:40 +0500

    Add files via upload

---
+++ admin/delete-category.php
@@ -0,0 +1,15 @@
+
+<?php
+	require('../common/functions.php');
+	if(isset($_GET['catid'])){
+		$catid=(int)$_GET['catid'];
+		$deleteRes=delete_category($catid);
+		if($deleteRes['bool']==true){
+			$_SESSION['msg']='<p class="alert alert-success">Category has been deleted.</p>';
+			header("location:category.php");
+		}else{
+			$_SESSION['msg']='<p class="alert alert-warning">Category has not been deleted.</p>';
+			header("location:category.php");
+		}
+	}
+?>
